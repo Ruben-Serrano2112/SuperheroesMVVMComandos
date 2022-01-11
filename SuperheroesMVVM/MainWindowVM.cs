62136e6d576969eb9111449db028eabb1dd3b49d
Ruben <ruben.serrano@iesdoctorbalmis.com>
2022-01-11 14:34:57 +0100

    Agregar archivos de proyecto.

---
+++ SuperheroesMVVM/MainWindowVM.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.Toolkit.Mvvm.ComponentModel;
+using Microsoft.Toolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperheroesMVVM
+{
+    class MainWindowVM : ObservableObject
+    {
+        private List<Superheroe> heroes;
+
+        private Superheroe heroeActual;
+
+        public Superheroe HeroeActual
+        {
+            get { return heroeActual; }
+            set 
+            {
+                SetProperty(ref heroeActual, value);
+            }
+        }
+
+        private int total;
+
+        public int Total
+        {
+            get { return total; }
+            set 
+            {
+                SetProperty(ref total, value);
+            }
+        }
+
+        private int actual;
+
+        public int Actual
+        {
+            get { return actual; }
+            set 
+            {
+                SetProperty(ref actual, value);
+            }
+        }
+
+        private RelayCommand siguienteComand;
+
+        public RelayCommand SiguienteComand
+        {
+            get { return siguienteComand; }
+            set { SetProperty(ref siguienteComand, value); }
+        }
+
+        private RelayCommand retrocederComand;
+
+        public RelayCommand RetrocederComand
+        {
+            get { return retrocederComand; }
+            set { SetProperty(ref retrocederComand, value); }
+        }
+
+        ListaSuperheroes lista = new ListaSuperheroes();
+
+        public MainWindowVM()
+        {
+
+            heroes = lista.GetSamples();
+            HeroeActual = heroes[0];
+            Total = heroes.Count;
+            Actual = 1;
+            SiguienteComand = new RelayCommand(Siguiente);
+            RetrocederComand = new RelayCommand(Anterior);
+        }
+
+        
+
+        public void Siguiente()
+        { 
+            if (Actual < Total)
+            {
+                Actual++;
+                HeroeActual = heroes[Actual-1];
+            }
+        }
+
+        public void Anterior()
+        {
+            if (Actual > 1)
+            {
+                Actual--;
+                HeroeActual = heroes[Actual-1];
+            }
+        }
+    }
+}
---
+++ SuperheroesMVVM/Superheroe.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.Toolkit.Mvvm.ComponentModel;
+using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace SuperheroesMVVM
+{
+    class Superheroe : ObservableObject
+    {
+        private string _nombre;
+        public string Nombre
+        {
+            get => _nombre;
+            set
+            {
+                if (_nombre != value)
+                {
+                    SetProperty(ref _nombre, value);
+                }
+            }
+        }
+        private string _imagen;
+        public string Imagen
+        {
+            get => _imagen;
+            set
+            {
+                SetProperty(ref _imagen, value);
+            }
+        }
+
+        private bool _vengador;
+        public bool Vengador
+        {
+            get => _vengador;
+            set
+            {
+                if (_vengador != value)
+                {
+                    SetProperty(ref _vengador, value);
+                }
+            }
+        }
+
+        private bool _xmen;
+        public bool Xmen
+        {
+            get => _xmen;
+            set
+            {
+                if (_xmen != value)
+                {
+                    SetProperty(ref _xmen, value);
+                }
+            }
+        }
+
+        private bool _heroe;
+        public bool Heroe
+        {
+            get => _heroe;
+            set
+            {
+                if (_heroe != value)
+                {
+                    SetProperty(ref _heroe,value);
+                }
+            }
+        }
+
+        public Superheroe()
+        {
+        }
+
+        public Superheroe(string nombre, string imagen, bool vengador, bool xmen, bool heroe)
+        {
+            Nombre = nombre;
+            Imagen = imagen;
+            Vengador = vengador;
+            Xmen = xmen;
+            Heroe = heroe;
+        }
+
+        
+    }
+}
